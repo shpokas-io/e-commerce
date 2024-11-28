29fe4995538075c5da5c38763717f9405ce134e8
shpokas <s.shpokas@gmail.com>
2024-11-28 09:46:54 +0200

    feat: completed product management flow

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
+import { ProductsModule } from './products/products.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule],
+  imports: [DatabaseModule, AuthModule, ProductsModule],
   controllers: [],
   providers: [],
 })
