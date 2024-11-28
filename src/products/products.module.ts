dec4d1c99964029e5709d1572b9a0a2825542553
shpokas <s.shpokas@gmail.com>
2024-11-28 13:13:13 +0200

    Fix: Import AuthModule in ProductsModule to resolve JwtService dependency issue

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { ProductsService } from './products.service';
 import { DatabaseModule } from 'src/database/database.module';
 import { ProductsController } from './products.controller';
+import { AuthModule } from 'src/auth/auth.module';
 
 @Module({
-  imports: [DatabaseModule],
+  imports: [DatabaseModule, AuthModule],
   controllers: [ProductsController],
   providers: [ProductsService],
 })
