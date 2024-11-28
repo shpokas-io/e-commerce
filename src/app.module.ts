0cfe712f90cab87c6049711b1f9a91aa0a1b5b36
shpokas <s.shpokas@gmail.com>
2024-11-28 11:38:11 +0200

    chore: registered profile module in app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { DatabaseModule } from './database/database.module';
 import { AuthModule } from './auth/auth.module';
 import { ProductsModule } from './products/products.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [DatabaseModule, AuthModule, ProductsModule],
+  imports: [DatabaseModule, AuthModule, ProductsModule, ProfileModule],
   controllers: [],
   providers: [],
 })
