619554ba38a4016c221a7c9735887a320a2e7071
shpokas <s.shpokas@gmail.com>
2024-12-05 08:48:25 +0200

    chore: register the new service for populating products

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -3,10 +3,11 @@ import { ProductsService } from './products.service';
 import { DatabaseModule } from 'src/database/database.module';
 import { ProductsController } from './products.controller';
 import { AuthModule } from 'src/auth/auth.module';
+import { PopulateProductsService } from './populate-products';
 
 @Module({
   imports: [DatabaseModule, AuthModule],
   controllers: [ProductsController],
-  providers: [ProductsService],
+  providers: [ProductsService, PopulateProductsService],
 })
 export class ProductsModule {}
